1c3c28242a361f502b2c1a991de4b3d3a38ceddf
cmcoke <minoru.cm@gmail.com>
2022-08-21 17:12:09 -0500

    created the testimonials section of the website.

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -3,13 +3,12 @@ import styles, { layout } from "../style";
 
 const Billing = () => (
   <section id="product" className={layout.sectionReverse}>
-
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
       {/* gradient start */}
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-      <div className="absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
       {/* gradient end */}
     </div>
 
@@ -29,7 +28,7 @@ const Billing = () => (
         <img src={google} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
       </div>
     </div>
-
   </section>
 );
-export default Billing
+
+export default Billing;
